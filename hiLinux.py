86519db7d9d9c1f8a80eabfcd0876d5680e260e3
Aditya Minz <98016435+Adityaminz18@users.noreply.github.com>
2024-04-25 06:24:28 +0530

    Update hiLinux.py

--- hiLinux.py
+++ hiLinux.py
@@ -5,6 +5,7 @@ os.system('''wget https://github.com/cloudflare/cloudflared/releases/latest/down
 sudo dpkg -i cloudflared-linux-amd64.deb
 sudo apt install php -y
 sudo apt install tail -y
+sudo apt install gnome-terminal
 rm -rf hiLinux.py cloudflared-linux-amd64.deb
 clear
 echo "run python3 PhishYou.py"''')
